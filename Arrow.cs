ea5bfc59b0ca576814df87728067b00524b481c3
lolpigg <rostikmostik68@gmail.com>
2022-11-29 22:10:57 +0300

    ssa

--- Arrow.cs
+++ Arrow.cs
@@ -8,24 +8,62 @@ namespace Folder
 {
     class Arrow
     {
-        int Max, Min, Cur;
-        string ArrowStr = "->";
-        public Arrow(int Max, int Min, int Cur)
+        public static int Max, Min, Cur;
+        public static int Pos;
+        public static string ArrowStr = "->";
+        public Arrow(int min, int max)
         {
-            this.Cur = Min;
-            this.Max = Max;
-            this.Min = Min;
+            Cur = min;
+            Max = max;
+            Min = min;
         }
-        public static void ArrowAct(string Where)
+        public static int ArrowAct()
         {
-            if (Where == "Up")
+            ConsoleKeyInfo key;
+            Pos = Min;
+            do
             {
-
-            }
-            else if (Where == "Down")
-            {
-
-            }
+                key = Console.ReadKey(true);
+                Console.SetCursorPosition(0, Pos);
+                Console.Write("  ");
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Pos--;
+                        if (Pos < Min)
+                        {
+                            Pos = Max;
+                        }
+                        else if (Pos > Max)
+                        {
+                            Pos = Min;
+                        }
+                        Console.SetCursorPosition(0, Pos);
+                        Console.Write(ArrowStr);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (Pos < Min)
+                            {
+                                Pos = Max;
+                            }
+                            if (Pos > Max)
+                            {
+                                Pos = Min;
+                            }
+                            Pos++;
+                            Console.SetCursorPosition(0, Pos);
+                            Console.Write(ArrowStr);
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        Pos = -1000;
+                        return Pos;
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Enter);
+            Console.Clear();
+            return Pos;
         }
     }
 }
--- Folders.cs
+++ Folders.cs
@@ -8,8 +8,17 @@ namespace Folder
 {
     static class Folders
     {
-        public static void Disk()
+        public static void Process()
         {
+            do
+            {
+                string DiskPath = Disk();
+                string PapkaPath = Papka(DiskPath);
+            } while (true);
+        }
+        public static string Disk()
+        {
+            Console.Clear();
             int i = 0;
             Console.WriteLine($"Добро пожаловать в проводник! Выберите диск.\n");
             DriveInfo[] disks = DriveInfo.GetDrives();
@@ -18,27 +27,40 @@ namespace Folder
                 Console.WriteLine($"  {disk.Name} Свободно {disk.TotalFreeSpace / 1073741824}ГБ из {disk.TotalSize / 1073741824}ГБ.");
                 i++;
             }
-            Operation();
+            Arrow Arrow = new Arrow(i - 1, i);
+            int Pos = Arrow.ArrowAct() - 2;
+            return disks[Pos].Name;
         }
-        private static void Operation()
+        static private string Papka(string Path)
         {
-            do
+            int FileNum = 0;
+            Console.WriteLine("Выбраны папки.\n");
+            List<string> allElements = new List<string>();
+            var PapkaInf = new DirectoryInfo(Path);
+            if (Path.Contains("."))
             {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                switch (key.Key)
-                {
-                    case ConsoleKey.Escape:
-                        break;
-                    case ConsoleKey.Enter:
-                        break;
-                    case ConsoleKey.UpArrow:
-                        Arrow.ArrowAct("Up");
-                        break;
-                    case ConsoleKey.DownArrow:
-                        Arrow.ArrowAct("Down");
-                        break;
-                }
-            } while (true);
+                return Path;
+            }
+            string[] allDirectories = Directory.GetDirectories(Path);
+            string[] allFiles = Directory.GetFiles(Path);
+            allElements.AddRange(allDirectories);
+            allElements.AddRange(allFiles);
+            allElements.Sort();
+            foreach (string elements in allElements)
+            {
+                Console.WriteLine($"  {elements}");
+                Console.SetCursorPosition(90, FileNum + 2);
+                Console.WriteLine($"  {PapkaInf.CreationTime}");
+                FileNum++;
+            }
+            Arrow menu = new Arrow(1, FileNum - 1);
+            int chosenPosition = Arrow.ArrowAct() - 2;
+            if (chosenPosition == -100)
+            {
+                Console.Clear();
+                return string.Empty;
+            }
+            return Papka(allElements[chosenPosition]);
         }
     }
 }
